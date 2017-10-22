99b6006fee4e1ad79c486a14c9a6479ce4c87a6f
Romain Lacube <romain.lacube@gmail.com>
2017-10-22 16:50:33 +0200

    iteration 3 terminée

---
+++ Iteration_2/Srcs/actions/LoginAction.inc.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once("actions/Action.inc.php");
+
+class LoginAction extends Action {
+
+	/**
+	 * Traite les données envoyées par le visiteur via le formulaire de connexion
+	 * (variables $_POST['nickname'] et $_POST['password']).
+	 * Le mot de passe est vérifié en utilisant les méthodes de la classe Database.
+	 * Si le mot de passe n'est pas correct, on affiche le message "Pseudo ou mot de passe incorrect."
+	 * Si la vérification est réussie, le pseudo est affecté à la variable de session.
+	 *
+	 * @see Action::run()
+	 */
+	public function run() {
+  	/* TODO START */
+        $nickname = $_POST['nickname'];
+        if ($this->database->checkPassword($nickname, $_POST['password']) === true) {
+            $this->setView(getViewByName("Message"));
+            $this->getView()->setMessage("Vous êtes connecté");
+            $this->setSessionLogin($nickname);
+        } else {
+            $this->setView(getViewByName("Message"));
+            $this->getView()->setMessage("Pseudo ou mot de passe incorrect.");
+        }
+
+  	/* TODO END */
+	}
+
+}
+
+?>
